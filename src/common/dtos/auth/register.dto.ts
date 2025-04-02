d1887790bb6c909cf414109e8004d1d594f87716
Thalles Domician <thalles.oliveira@cialdnb.com>
2025-04-01 23:54:09 -0300

    feat(dto): ✨ add registration and sync push data transfer objects
    adds validation for email and password fields in the registration DTO to ensure they are not empty.
    
    introduces a new sync push DTO that includes arrays for workouts, measurements, exercises, and templates, each with validation for nested objects.

--- src/common/dtos/auth/register.dto.ts
+++ src/common/dtos/auth/register.dto.ts
@@ -2,9 +2,11 @@ import { IsEmail, IsNotEmpty, MinLength, IsOptional } from 'class-validator';
 
 export class RegisterDto {
   @IsEmail()
+  @IsNotEmpty()
   email: string;
 
   @MinLength(6)
+  @IsNotEmpty()
   password: string;
 
   @IsOptional()
