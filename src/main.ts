0a8ceb1ec25de38371d582b41df667f3e60bdae0
Thalles Domician <thalles.oliveira@cialdnb.com>
2025-04-07 00:52:13 -0300

    feat(auth): 🚀 enhance user registration and login
    adds conflict handling for existing users during registration
    improves error messages with internationalization
    returns user details along with the token on login and registration

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(process.env.PORT || 3000);
+  await app.listen(process.env.PORT || 3000, '0.0.0.0');
+  console.log(`Server is running on port ${process.env.PORT || 3000}`);
 }
 bootstrap();
--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,17 +1,37 @@
-import { Injectable, UnauthorizedException } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { PrismaService } from '../../prisma/prisma.service';
 import * as bcrypt from 'bcrypt';
+import { I18nService } from 'nestjs-i18n';
 
 @Injectable()
 export class AuthService {
   constructor(
     private jwtService: JwtService,
     private prisma: PrismaService,
+    private i18n: I18nService,
   ) {}
 
   async register(data: { email: string; password: string; name?: string }) {
     const hashedPassword = await bcrypt.hash(data.password, 10);
+
+    const existingUser = await this.prisma.user.findUnique({
+      where: { email: data.email },
+    });
+    if (existingUser) {
+      throw new ConflictException(
+        this.i18n.t('common.errors.conflict', {
+          args: {
+            entity: 'user',
+          },
+        }),
+      );
+    }
+
     const user = await this.prisma.user.create({
       data: {
         email: data.email,
@@ -20,7 +40,7 @@ export class AuthService {
       },
     });
     const token = this.generateToken(user.id, user.email);
-    return { token };
+    return { token, user: { id: user.id, email: user.email, name: user.name } };
   }
 
   async login(data: { email: string; password: string }) {
@@ -28,10 +48,20 @@ export class AuthService {
       where: { email: data.email },
     });
     if (!user || !(await bcrypt.compare(data.password, user.password))) {
-      throw new UnauthorizedException('Invalid credentials');
+      throw new UnauthorizedException(
+        this.i18n.t('common.errors.invalidCredentials'),
+      );
     }
     const token = this.generateToken(user.id, user.email);
-    return { token };
+    return {
+      token,
+      user: {
+        id: user.id,
+        email: user.email,
+        name: user.name,
+        avatarUrl: user.avatarUrl,
+      },
+    };
   }
 
   async getMe(user: { userId: string }) {
